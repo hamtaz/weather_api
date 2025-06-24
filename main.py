a4e298db2d21e75bb3aeb7f29ed00b3ebab70c01
Hamzat Tazabaev <hamzat.tazabaev@gmail.com>
2025-06-24 14:38:58 +0200

    Added redis caching + changes to code structure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import redis
-from redis.cache import CacheConfig
+import sys
+
+redis_client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
 class WeatherData:
     def __init__(self, day, country, temp, description):
         self.day = day
@@ -8,21 +11,41 @@ class WeatherData:
         self.temp = temp
         self.description = description
         
-    def __str__(self):
-        return f"\nDay: {self.day}\nCity / Country: {self.country}\nTemperature (Celcius): {self.temp}\nDesc: {self.description}\n"
+    def to_dict(self):
+        return {
+            "Day": self.day,
+            "Country": self.country,
+            "Temp": self.temp,
+            "Description": self.description
+        }
 
-def get_api():
-    place = input("Enter Address, partial address, or lat,lon: ")
+def get_weather():
+    place = input("Enter Address, partial address, or lat,lon / quit to exit: ")
+    
+    if place.lower() == "quit":
+        sys.exit()
+    
+    cached_data = redis_client.get(f"weather:{place}")
+    if cached_data:
+        print("Data loaded from cache")
+        return cached_data
+    
     try:
         api_link = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{place}?unitGroup=metric&include=current&key=LM4GMBNWSGYADVYXVUNVNET8V&contentType=json"
     
         response = requests.get(api_link)
         data = response.json()
-    
-        weather_data = WeatherData(data["days"][0]["datetime"], data["days"][1]["description"], data["resolvedAddress"], data["days"][0]["temp"])
-        return weather_data
+        weather_data = WeatherData(
+            data["days"][0]["datetime"], 
+            data["resolvedAddress"], 
+            data["days"][0]["temp"], 
+            data["days"][0]["description"]
+        )
+        
+        weather_dict = weather_data.to_dict()
+        redis_client.setex(f"weather:{place}", 900, str(weather_dict))
+        return weather_dict
     except:
-        return f"Connection Error {response.status_code}"
-
-
-print(get_api())
+        return f"Error code: {response.status_code}"
+while True:
+    print(get_weather())
